bd1ec8886c73854097b1eaed05ace43cdc786986
DataNick <DataNick@users.noreply.github.com>
2015-09-07 18:59:47 -0400

    Added packages autoform, meteor collection2 and made posts.html show data from database

--- client/common.js
+++ client/common.js
@@ -1,6 +1,15 @@
 Meteor.startup(function () {
   AccountsEntry.config({
     homeRoute: '/',                    // mandatory - path to redirect to after sign-out
-    dashboardRoute: '/'      // mandatory - path to redirect to after successful sign-in
+    dashboardRoute: '/',      // mandatory - path to redirect to after successful sign-in
+    passwordSignupFields: 'USERNAME_AND_EMAIL'
   });
+  Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_AND_EMAIL'
+  })
+});
+
+Template.registerHelper('getProfileImg', function(userId){
+  var imgUrl = UserImages.findOne({userId: userId}).image;
+  return imgUrl;
 });
